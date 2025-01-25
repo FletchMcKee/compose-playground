c1bd5d061116170e11d5608b1ad25c9dfbd8ca24
FletchMcKee <cfmckee.dev@gmail.com>
2025-01-25 09:53:46 -0600

    Added spotless and ktlint. Will add a build check later.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,30 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
     alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.spotless) apply false
+}
+
+subprojects {
+    apply(plugin = "com.diffplug.spotless")
+    configure<com.diffplug.gradle.spotless.SpotlessExtension> {
+        kotlin {
+            target("src/**/*.kt")
+            ktlint(libs.ktlint.cli.get().version).editorConfigOverride(
+              mapOf(
+                "ktlint_standard_function-expression-body" to "disabled",
+                "ktlint_standard_filename" to "disabled",
+                "ktlint_function_naming_ignore_when_annotated_with" to "Composable",
+              ),
+            ).customRuleSets(
+              listOf(
+                libs.ktlint.compose.rules.get().toString()
+              )
+            )
+        }
+
+      kotlinGradle {
+        target("src/**/*.kt")
+        ktlint(libs.ktlint.cli.get().version)
+      }
+    }
 }
